2cb7d9de558efb935cc66982cc6e05e3b74e42d5
cferdinandi <chris@gomakethings.com>
2023-10-13 17:37:03 -0400

    Updated build, removed focus() method

--- dist/reef.es.js
+++ dist/reef.es.js
@@ -614,40 +614,5 @@ function component (elem, template, options = {}) {
 	return new Component(elem, template, options);
 }
 
-// Hold the selector for the element to focus o
-let focusOn;
-
-/**
- * Set focus on the element
- * 1ms delay prevents odd bugs from browser-native focus shifts
- */
-function setFocus () {
-	setTimeout(function () {
-
-		// Find target element in the DOM
-		let elem = document.querySelector(focusOn);
-		focusOn = null;
-		if (!elem) return;
-
-		// Try to focus element
-		elem.focus();
-		if (document.activeElement === elem) return;
-
-		// If element could not be focused, set a tabindex and try again
-		elem.setAttribute('tabindex', -1);
-		elem.focus();
-
-	}, 1);
-}
-
-/**
- * Shift focus to trigger a screen reader announcement after content is loaded
- */
-function focus (selector) {
-	if (!selector || typeof selector !== 'string') return;
-	focusOn = selector;
-	document.addEventListener('reef:render', setFocus, {once: true});
-}
-
-export { component, focus, render, signal, store };
+export { component, render, signal, store };
 //# sourceMappingURL=reef.es.js.map
--- src/reef.js
+++ src/reef.js
@@ -2,6 +2,5 @@ import signal from './components/signal.js';
 import store from './components/store.js';
 import render from './components/render.js';
 import component from './components/component.js';
-import focus from './components/focus.js';
 
-export {signal, store, render, component, focus};
+export {signal, store, render, component};
