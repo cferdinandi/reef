cf1cd9d7e07500739be51ade272e1fadfb7ee646
cferdinandi <chris@gomakethings.com>
2023-10-13 23:20:01 -0400

    Added focus() method

--- dist/reef.cjs.js
+++ dist/reef.cjs.js
@@ -618,7 +618,43 @@ function component (elem, template, options = {}) {
 	return new Component(elem, template, options);
 }
 
+// Hold the selector for the element to focus on
+let focusOn;
+
+/**
+ * Set focus on the element
+ * 1ms delay prevents odd bugs from browser-native focus shifts
+ */
+function setFocus () {
+	setTimeout(function () {
+
+		// Find target element in the DOM
+		let elem = document.querySelector(focusOn);
+		focusOn = null;
+		if (!elem) return;
+
+		// Try to focus element
+		elem.focus();
+		if (document.activeElement === elem) return;
+
+		// If element could not be focused, set a tabindex and try again
+		elem.setAttribute('tabindex', -1);
+		elem.focus();
+
+	}, 1);
+}
+
+/**
+ * Shift focus to trigger a screen reader announcement after content is loaded
+ */
+function focus (selector) {
+	if (!selector || typeof selector !== 'string') return;
+	focusOn = selector;
+	document.addEventListener('reef:render', setFocus, {once: true});
+}
+
 exports.component = component;
+exports.focus = focus;
 exports.render = render;
 exports.signal = signal;
 exports.store = store;
